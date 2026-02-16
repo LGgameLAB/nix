fded9dca91f9da9956ef90c321d2ed4be03e423f
24apricots <michaelzls@berkeley.edu>
2026-02-16 00:22:16 -0800

    improve accuracy of logged_in_users_exporter.sh
    - prevent lingering/closing logind sessions from being listed; only
      list if online/active
    - locked status now checks if a session is locked, not just whether it
      is in the foreground

--- modules/ocf/logged-in-users-exporter/logged_in_users_exporter.sh
+++ modules/ocf/logged-in-users-exporter/logged_in_users_exporter.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 OUTPUT_FILE="/var/lib/node_exporter/textfile_collector/logged_in_users.prom"
 > "$OUTPUT_FILE"
+
 loginctl list-sessions --no-legend | while read -r session_id uid user seat leader class tty idle since; do
   if [[ $class == "user" ]] && [[ $seat == "seat0" ]] && [[ $idle == "no" ]]; then
-    state=$(loginctl show-session "$session_id" -p State --value)
-    if [[ $state == "active" ]]; then
-      locked_status="unlocked"
-    else
-      locked_status="locked"
+    # we only want to list users who are:
+    # - online: logged in but not in the foreground
+    # - active: logged in and in the foreground
+    # TODO: list logged in users but dont place a penguin if no sessions are active
+    session_status=$(loginctl show-session "$session_id" -p State --value)
+    if ! [[ $session_status == "online" || $session_status == "active" ]]; then
+      continue
     fi
-  echo "node_logged_in_user{name=\"$user\", state=\"$locked_status\"} 1" > $OUTPUT_FILE
+
+    # If the locking program supports systemd-logind, LockedHint can checked instead.
+    # To support lockers that do not support logind, locked_status is only set to
+    # locked if LockedHint specifically says so.
+    session_lockedhint=$(loginctl show-session "$session_id" -p LockedHint --value)
+    if [[ $session_lockedhint == "yes" ]]; then
+      locked_status="locked"
+    else
+      locked_status="unlocked"
+    fi
+
+    echo "node_logged_in_user{name=\"$user\", state=\"$locked_status\"} 1" > $OUTPUT_FILE
   fi
 done
